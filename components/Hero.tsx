013987045d4475e0210709f13744e36e8df3493e
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-10-23 19:07:07 +0530

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,22 +8,24 @@ const Hero = () => {
     }
     return (
         <div className='hero'>
-            <div className='flex-1 pt-36 padding-x'>
+            <div className='flex-1 w-full h-screen relative'>
+                <Image alt='Image' src={'https://cdn.pixabay.com/photo/2019/03/31/14/31/houses-4093227_1280.jpg'} width={1000} height={500} className='w-full  absolute' />
+                {/* <div className='hero__image-container'>
+                    <div className="hero__image">
+                        <Image src="https://cdn.pixabay.com/photo/2019/03/31/14/31/houses-4093227_1280.jpg" alt="hero" fill className='object-contain' />
+                    </div>
+                    <div className='hero__image-overlay' />
+
+                </div>
                 <h1 className='hero__title'>Find, Book and Rent a Car --- Quickly & Easily</h1>
                 <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process....</p>
                 <CustomButton
                     title="Explore Cars"
                     containerStyles="bg-primary-blue text-white rounded-full mt-10"
                     handleClick={handleScroll}
-                />
+                /> */}
             </div>
-            <div className='hero__image-container'>
-                <div className="hero__image">
-                    <Image src="/hero2.webp" alt="hero" fill className='object-contain' />
-                </div>
-                <div className='hero__image-overlay' />
 
-            </div>
 
         </div>
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,21 +6,25 @@ import { CustomButton } from '.'
 
 const Navbar = () => {
     return (
-        <header className='w-full absolute z-10'>
+        <header className='w-full absolute z-10 bg-gray-100 bg-opacity-60'>
             <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
-                <Link href={"/"} className='flex justify-center items-center'>
-                    <Image src={"/logo2.png"}
-                        alt='Car Hub Logo'
-                        width={200}
-                        height={50}
-                        className='object-contain'
+                <div>
+                    <Link href={"/"} className='flex justify-center items-center'>
+                        <h1 className='text-primary-blue text-2xl font-bold'>Traveler Stop</h1>
+                    </Link>
+                </div>
+                <div className='flex'>
+                    <CustomButton
+                        title='Cart'
+                        btnType='button'
+                        containerStyles=' font-bold text-white rounded-full bg-primary-blue min-w-[130px]'
                     />
-                </Link>
-                <CustomButton
-                    title='Sign In'
-                    btnType='button'
-                    containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
-                />
+                    <CustomButton
+                        title='Sign In'
+                        btnType='button'
+                        containerStyles=' font-bold text-primary-blue rounded-full bg-white min-w-[130px]'
+                    />
+                </div>
 
             </nav>
 
